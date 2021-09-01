76e8239d5e931dbdebcbef220fa62eb9970affa2
Robin <alegalone@gmail.com>
2021-09-02 07:55:06 +0800

    DesignPattern first commit

---
+++ Learning/Program.cs
@@ -0,0 +1,21 @@
+﻿using DesignPatternCoffeeShop;
+using System;
+
+namespace Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beverage beverage = new HouseBlend();
+            beverage = new Sugar(beverage);
+            beverage = new Milk(beverage);
+            OrderResult(beverage);
+        }
+        public static void OrderResult(Beverage beverage)
+        {
+            Console.WriteLine($"您的飲料：{beverage.GetDescription()}");
+            Console.WriteLine($"您的金額：{beverage.Cost()}");
+        }
+    }
+}
